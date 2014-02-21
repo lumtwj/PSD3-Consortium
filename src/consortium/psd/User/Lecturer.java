28cdb5f00b3f354407e6234a779f835d9c69b4b8
Kevin Pang <kbypang@gmail.com>
2014-02-22 01:54:28 +0800

    VER 1.32 - Re-Push

---
+++ src/consortium/psd/User/Lecturer.java
@@ -0,0 +1,37 @@
+package consortium.psd.User;
+
+import java.util.ArrayList;
+
+import consortium.psd.Course.CourseCF;
+import consortium.psd.Timetable.Timetable;
+
+public class Lecturer extends User
+{
+	ArrayList<Timetable> timetables;
+	ArrayList<CourseCF> courses;
+	
+	/**
+	 * @return the timetables
+	 */
+	public ArrayList<Timetable> getTimetables() {
+		return timetables;
+	}
+	/**
+	 * @param timetables the timetables to set
+	 */
+	public void setTimetables(ArrayList<Timetable> timetables) {
+		this.timetables = timetables;
+	}
+	/**
+	 * @return the courses
+	 */
+	public ArrayList<CourseCF> getCourses() {
+		return courses;
+	}
+	/**
+	 * @param courses the courses to set
+	 */
+	public void setCourses(ArrayList<CourseCF> courses) {
+		this.courses = courses;
+	}
+}
