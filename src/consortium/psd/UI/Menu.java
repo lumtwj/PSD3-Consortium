e999b4a476e3e0d59e42503b00e0a69b8d32a317
Joe Lum <lumtwj@gmail.com>
2014-02-23 20:13:15 +0800

    VER 1.61 - Remove unnecessary method

--- src/consortium/psd/UI/Menu.java
+++ src/consortium/psd/UI/Menu.java
@@ -337,7 +337,7 @@ public class Menu {
 			System.out.println("4.\tDelete Room");
 			System.out.println("0.\tBack");
 
-			newMenuRoom(getInputForMenu());
+			menuRoom(getInputForMenu());
 
 			break;
 		}
@@ -349,7 +349,7 @@ public class Menu {
 		return s.nextInt();
 	}
 
-	private void newMenuRoom(int selection) {
+	private void menuRoom(int selection) {
 		RoomController rc = new RoomController();
 		Scanner sc = new Scanner(System.in);
 
@@ -497,153 +497,6 @@ public class Menu {
 		default:
 			System.out.println("Invalid options!\n");
 			displaySubMenu(5);
-			break;
-		}
-
-		//sc.close();
-	}
-
-	private void menuRoom(int c) {
-		RoomController rc = new RoomController();
-		Scanner sc = new Scanner(System.in);
-
-		switch (c) {
-		case 1:
-			boolean flag = true;
-			while (flag) {
-				rc.viewRoom();
-				System.out
-				.println("Which room do you want to view. Key 0 to exit");
-				int room = s.nextInt();
-				if (room <= rc.getsize()) {
-					if (room == 0) {
-						flag = false;
-					} else {
-						rc.viewRoom(room - 1);
-					}
-				} else {
-					System.out.println("Value out of bound");
-				}
-			}
-			break;
-
-		case 2:
-			System.out.println("You have chosen to add a room");
-			System.out.println("Enter the name of the room");
-			String name = sc.nextLine();
-			System.out.println("Enter the location of the room");
-			String location = sc.nextLine();
-
-			boolean notint = true;
-			while (notint) {
-				System.out.println("Enter the max capacity of the room");
-				int cap = s.nextInt();
-				//if (isInteger(cap)) {
-				notint = false;
-				rc.addRoom(name, location, cap);
-				/*} else {
-					System.out
-					.println("The number you keyed in is not numeric figures, please try again!");
-				}*/
-			}
-			break;
-
-		case 3:
-			notint = true;
-			boolean notinta = true;
-			while (notint) {
-				rc.viewRoom();
-				System.out
-				.println("Please enter the id of the room you want to edit.");
-				String cap = sc.nextLine();
-				//if (isInteger(cap)) {
-				notint = false;
-				rc.viewRoom(Integer.parseInt(cap) - 1);
-				while (notinta) {
-					System.out
-					.println("Is this the room you want to edit? (yes | no)");
-					String ans = sc.nextLine();
-					if (ans.equals("yes")) {
-						System.out.println("The name of the classroom");
-						String n = sc.nextLine();
-						System.out
-						.println("The location of the classroom");
-						String l = sc.nextLine();
-						boolean notintb = true;
-						while (notintb) {
-							System.out
-							.println("The max occupancy of the room?");
-							String capa = sc.nextLine();
-							//if (isInteger(capa)) {
-							rc.editRoom(Integer.parseInt(cap) - 1,
-									n, l, Integer.parseInt(capa));
-							System.out
-							.println("The room have been edit!");
-							notintb = false;
-							/*} else {
-								System.out.println("Invalid input");
-							}*/
-						}
-
-						notinta = false;
-					} else if (ans.equals("no")) {
-						System.out.println("Exiting to main menu.");
-						notinta = false;
-					} else {
-						System.out.println("Invalid input");
-					}
-				}
-
-				/*} else {
-					System.out
-					.println("The number you keyed in is not numeric figures, please try again!");
-				}*/
-			}
-			break;
-
-		case 4:
-			notint = true;
-			notinta = true;
-			while (notint) {
-				rc.viewRoom();
-				System.out
-				.println("Please enter the id of the room you want to delete.");
-				int cap = s.nextInt();
-				//if (isInteger(cap)) {
-				notint = false;
-				rc.viewRoom(cap - 1);
-				while (notinta) {
-					System.out
-					.println("Is this the room you want to delete? (yes | no)");
-					String ans = s.nextLine();
-					if (ans.equals("yes")) {
-						rc.delRoom(cap - 1);
-						System.out
-						.println("The room have been deleted!");
-						notinta = false;
-					} else if (ans.equals("no")) {
-						System.out.println("Item not deleted");
-						notinta = false;
-					} else {
-						System.out.println("Invalid input");
-					}
-				}
-
-				/*} else {
-					System.out
-					.println("The number you keyed in is not numeric figures, please try again!");
-				}*/
-			}
-			break;
-
-		case 0:
-			//exit = true;
-			break;
-
-		default:
-			System.out
-			.println("This is not a correct choice, please enter another choice.");
-			//showMenu(type);
 			break;
 		}
 	}
