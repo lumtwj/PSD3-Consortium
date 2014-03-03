ba349573b9acda75c87153760f3ef7323e9342d1
Joe Lum <lumtwj@gmail.com>
2014-03-03 15:36:28 +0800

    VER 2.20 - Update JUnit test class to support concurrent user login

--- src/consortium/psd/User/LoginControllerTest.java
+++ src/consortium/psd/User/LoginControllerTest.java
@@ -3,25 +3,21 @@ package consortium.psd.User;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-import java.util.Scanner;
+import java.sql.SQLException;
 
 import org.junit.Test;
 
 public class LoginControllerTest {
 	@Test
 	public void testGetUserStringString() {
-		try
-		{
-			Scanner sc = new Scanner(System.in);
-			LoginController lct = new LoginController();
-			System.out.println("enter your username");
-			String username = sc.nextLine();
-			System.out.println("enter your password");
-			String password = sc.nextLine();
-			assertNotNull("", lct.getUser(username, password));
-		}
-		catch(Exception ex){
-			fail(ex.getMessage());
+		LoginController lct = new LoginController();
+
+		try {
+			for (int i = 4; i <= 15; i++) {
+				assertNotNull("", lct.getUser("student" + i, "student" + i));
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			fail(e.getMessage());
 		}
 	}
 }
